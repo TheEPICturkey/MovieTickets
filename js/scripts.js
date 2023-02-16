6f9e64218509af03129cadd28c52793726a0c5ef
Brandon Spear <Bowdowntotheepicturkey@gmail.com>
2023-02-15 18:58:04 -0800

    updat js
    Co-authored-by: Billy Lee <codingbillylee@gmail.com>
    Co-authored-by: Kaleb Taylor <kaleb.taylor1976@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,18 @@ function priceCalc(age) {
 
 }
 
+function movieDisplay(movieTitle, age) {
+  if (movieTitle === "brandonBuilding") {
+    return brandonBuilding.movie + " Time: " + brandonBuilding.time + " Price : $" + brandonBuilding.price; 
+  } else if (movieTitle === "brandonBob") {
+    return brandonBob.movie + " Time: " + brandonBob.time + brandonBob.price + " Price: $";
+  } else if (movieTitle === "billysRevenge") {
+    return billysRevenge.movie + " Time: " + billysRevenge.time + " Price : $" + billysRevenge.price; 
+  } else {
+    return billyStrikes.movie + " Time: " + billyStrikes.time + " Price : $" + billyStrikes.price;
+  }
+}
+
 window.addEventListener("DOMContentLoaded", function() {
   const form = document.getElementById("btn");
   form.addEventListener("click", handleSubmission)
@@ -34,84 +46,15 @@ window.addEventListener("DOMContentLoaded", function() {
 function handleSubmission(event){
   event.preventDefault();
   let movie = document.querySelector("#movies");
-  let movieTitle = movie.options[movie.selectedIndex].text;
+  let movieTitle = movie.value;
   let age = document.querySelector("input[type='number']").value;
-
+  console.log(movieTitle);
+  
+  display(movieDisplay(movieTitle, age));
+  
 }
 
-
-
-// Ticket.prototype.calcPrice = function(){
-//   price = 20000; //formula here
-//   let ageMod = 1;
-//   let timeMod = 1;
-  
-//   if (this.age <= 13) {
-//     ageMod = 0.5;
-//   } else if (this.age >= 60) {
-//     ageMod = 0.75;
-//   } else if (this.age <60 || this.age >14){
-//     ageMod = 1;
-//   } else {
-//     window.alert("Are you even human?")
-//   }
-
-//   if (this.time >= "08:00:00" && this.time <= "17:00:00") {
-//     timeMod = 0.40;
-//   }
-// }
-
-//UI Logic
-
-// function Ticket(movie, movieAge, age, time) {
-//   this.movie = movie;
-//   this.movieAge = movieAge;
-//   this.age = age;
-//   this.time = time;
-// }
-
-// Ticket.prototype.calcPrice = function(){
-//   price = 20000; //formula here
-//   let ageMod = 1;
-//   let movieMod = 1;
-//   let timeMod = 1;
-
-//   if (this.movieAge === "old") {
-//     movieMod = 0.75;
-//   }
-  
-//   if (this.age <= 13) {
-//     ageMod = 0.5;
-//   } else if (this.age >= 60) {
-//     ageMod = 0.75;
-//   } else if (this.age <60 || this.age >14){
-//     ageMod = 1;
-//   } else {
-//     window.alert("foo");
-//   }
-
-
-//   if (this.time >= "08:00:00" && this.time <= "17:00:00") {
-//     timeMod = 0.75;
-//   }
-
-//   price = price * ageMod * movieMod * timeMod;
-//   return price;
-// };
-
-// //UI Logic
-// window.addEventListener("load", function() {
-//   const form = document.getElementById("btn");
-//   form.addEventListener("submit", handleSubmission)
-// })
-
-// function handleSubmission(event){
-//   event.preventDefault();
-//   const form = document.getElementById("ticket-form");
-//   const movie = form.movie;
-//   const age = form.age.value;
-//   const time = form.time.value;
-//   const movieAge = form.movie.value;
-
-//   console.log(movie + " " + age + " " + time + " " + movieAge);
-// }
+function display(info) {
+  let h4 = document.querySelector("#display");
+  h4.innerText = info;
+}
