d22750b1d001a9d06ea0d2ec02597dba5b3265a7
Brandon Spear <Bowdowntotheepicturkey@gmail.com>
2023-02-15 20:45:01 -0800

    updat js
    Co-authored-by: Billy Lee <codingbillylee@gmail.com>
    Co-authored-by: Kaleb Taylor <kaleb.taylor1976@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,6 @@ movieList.prototype.assignId = function() {
   return this.movieId;
 }
 
-
 function Ticket(movie, movieAge, time, price) {
   this.movie = movie;
   this.movieAge = movieAge;
@@ -22,19 +21,48 @@ let brandonBob= new Ticket("Brandon Bob!", 0, "10:00AM", 15);
 let billysRevenge = new Ticket("Billy's Revenge", 18, "8:30PM", 15);
 let billyStrikes = new Ticket("Billy Strikes", 5, "1:30PM", 15);
 
-function priceCalc(age) {
-
+function title(movieTitle) {
+  if (movieTitle === "brandonBuilding") {
+    return brandonBuilding.movie
+  } else if (movieTitle === "brandonBob") {
+    return brandonBob.movie
+  } else if (movieTitle === "billysRevenge") {
+    return billysRevenge.movie
+  } else {
+    return billyStrikes.movie
+  }
 }
 
-function movieDisplay(movieTitle, age) {
+function times(movieTitle) {
   if (movieTitle === "brandonBuilding") {
-    return brandonBuilding.movie + " Time: " + brandonBuilding.time + " Price : $" + brandonBuilding.price; 
+    return brandonBuilding.time
   } else if (movieTitle === "brandonBob") {
-    return brandonBob.movie + " Time: " + brandonBob.time + brandonBob.price + " Price: $";
+    return brandonBob.time
   } else if (movieTitle === "billysRevenge") {
-    return billysRevenge.movie + " Time: " + billysRevenge.time + " Price : $" + billysRevenge.price; 
+    return billysRevenge.time
   } else {
-    return billyStrikes.movie + " Time: " + billyStrikes.time + " Price : $" + billyStrikes.price;
+    return billyStrikes.time
+  }
+}
+
+function prices(movieTitle, age) {
+  let priceOfMovie = 0;
+  let mod = 0.8;
+
+  if (movieTitle === "brandonBuilding") {
+    priceOfMovie = brandonBuilding.price;
+  } else if (movieTitle === "brandonBob") {
+    priceOfMovie = brandonBob.price;
+  } else if (movieTitle === "billysRevenge") {
+    priceOfMovie = billysRevenge.price;
+  } else {
+    priceOfMovie = billyStrikes.price;
+  }
+
+  if (age <= 10 || age >= 60) {
+    return priceOfMovie * mod;
+  } else {
+    return priceOfMovie;
   }
 }
 
@@ -47,14 +75,50 @@ function handleSubmission(event){
   event.preventDefault();
   let movie = document.querySelector("#movies");
   let movieTitle = movie.value;
+  let price = Ticket
   let age = document.querySelector("input[type='number']").value;
-  console.log(movieTitle);
-  
-  display(movieDisplay(movieTitle, age));
-  
+``  
+  displayInfo(title(movieTitle), times(movieTitle), prices(movieTitle, age));
 }
 
-function display(info) {
-  let h4 = document.querySelector("#display");
-  h4.innerText = info;
+function displayInfo(title, time, price) {
+  let h4Title = document.querySelector("#title");
+  let h4Time = document.querySelector("#time");
+  let h4Price = document.querySelector("#price");
+
+  h4Title.innerText = title;
+  h4Time.innerText = "Times: " + time;
+  h4Price.innerText = "Price: $" + price;
 }
+
+// function handleSubmission(event){
+//   event.preventDefault();
+//   const orderDisplay = document.getElementById("Ticket");
+//   orderDisplay.innerHTML="";
+//   const myCost = listCost(myOrder);
+//   displayOrder(myOrder, myCost);
+//   displayOrder(cost);
+// }
+// function listCost (convertedArray) {
+  //   const listArray = []
+  //   convertedArray.forEach(function(element){
+  //   switch (element) {
+  //     case("brandonBuilding"):
+  //       listArray.push("15.00")
+  //       break;
+  //     case("brandonBob"):
+  //       listArray.push("15.00")
+  //       break;   
+  //     case("billysRevenge"):
+  //       listArray.push("15.00")
+  //       break;   
+  //     case("billyStrikes"):
+  //       listArray.push("15.00")
+  //       break;
+  //     default:
+  //       listArray.push("20.00");
+  //       console.log("pushing" + element)
+  //     }
+  //   });
+  //   return(listArray);
+  // }
